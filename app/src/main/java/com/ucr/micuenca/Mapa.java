852feb8f908bbfa39c3338d1849e5a9660ff1091
Manuel Arroyo <Joe@localhost.localdomain>
2019-03-02 20:50:02 -0600

    base creo que todo la base ya esta

---
+++ app/src/main/java/com/ucr/micuenca/Mapa.java
@@ -0,0 +1,60 @@
+package com.ucr.micuenca;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class Mapa extends Activity {
+
+    private MapView mMapView;
+
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 9.029868;
+            double longitude = -83.050470;
+            int levelOfDetail = 11;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            showWebMap();
+        }
+    }
+
+    public static Mapa newInstance() {
+        return new Mapa();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+        mMapView = findViewById(R.id.mapView);
+        TextView titulo = findViewById(R.id.titulo);
+        titulo.setText("Mapa");
+        RelativeLayout menu = findViewById(R.id.titulo_menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(Mapa.this, Menu.class);
+                startActivity(next);
+            }
+        });
+
+        showWebMap();
+    }
+
+
+    private void showWebMap() {
+        String itemId = "453f5a7e73ba44c69090c150e3952dff";
+        String url = "https://www.arcgis.com/sharing/rest/content/items/" + itemId + "/data";
+        ArcGISMap map = new ArcGISMap(url);
+        mMapView.setMap(map);
+    }
+}
---
+++ app/src/main/java/com/ucr/micuenca/Menu.java
@@ -0,0 +1,95 @@
+package com.ucr.micuenca;
+
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class Menu extends Activity {
+
+    RelativeLayout mMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        FrameLayout mapa = findViewById(R.id.mapa);
+        final FrameLayout conceptos = findViewById(R.id.conceptos);
+        FrameLayout asadas  = findViewById(R.id.asadas);
+        FrameLayout zonificacion = findViewById(R.id.zonificacion);
+        FrameLayout datos = findViewById(R.id.datos);
+        final FrameLayout leyes = findViewById(R.id.leyes);
+        mapa.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(Menu.this, Mapa.class);
+                startActivity(next);
+            }
+        });
+
+        conceptos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(Menu.this, Mapa.class);
+                startActivity(next);
+            }
+        });
+
+        leyes.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(Menu.this, Mapa.class);
+                startActivity(next);
+            }
+        });
+
+        asadas.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(Menu.this, Mapa.class);
+                startActivity(next);
+            }
+        });
+        datos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(Menu.this, Mapa.class);
+                startActivity(next);
+            }
+        });
+
+        zonificacion.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(Menu.this, Mapa.class);
+                startActivity(next);
+            }
+        });
+
+        checkFirstRun();
+    }
+
+    public void checkFirstRun() {
+        boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isFirstRun", true);
+        if (isFirstRun){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Aviso Legal");
+            builder.setMessage("Esta aplicación es desarrollada por el Trabajo Comunal Universitario \n \t \"Gestión comunitaria del agua desde el manejo de cuencas hidrográficas\"\n7 mediante el uso de sistemas de información geográfica y software libre. Esta No debe ser utilizada con fines de lucro. Su contenido no tiene fuerza de ley, se dispone como insumo de carácter educativo y accionar social para las comunidades, entidades y personas interesadas.");
+            builder.setCancelable(true);
+            builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(), "Aceptado", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.show();
+
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                    .edit()
+                    .putBoolean("isFirstRun", false)
+                    .apply();
+        }
+    }
+}
