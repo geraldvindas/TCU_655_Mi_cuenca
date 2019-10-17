9139250278d148a0271ce12edc2fae0eb40069d4
geraldvindas <gerald.vindas@ucr.ac.cr>
2019-10-17 17:08:45 -0600

    Se agrega slider de Fernando

---
+++ app/src/main/java/com/ucr/micuenca/MapaSlider.java
@@ -0,0 +1,35 @@
+package com.ucr.micuenca;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.viewpager.widget.ViewPager;
+
+public class MapaSlider extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa_imagenes);
+
+        TextView titulo = findViewById(R.id.tv_titulo);
+        titulo.setText("Mapa");
+        RelativeLayout menu = findViewById(R.id.titulo_menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(MapaSlider.this, com.ucr.micuenca.Menu.class);
+                startActivity(next);
+            }
+        });
+
+        ViewPager viewPager = findViewById(R.id.activity_mapa_slider);
+        MapaImagenes mapa_imagenes = new MapaImagenes(this);
+        viewPager.setAdapter(mapa_imagenes);
+    }
+}
